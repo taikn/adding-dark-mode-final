a4deec8cfea1b142a8506c77e8829a98b96811c1
Edik <taikn@outlook.com>
2020-05-27 11:18:40 +0200

    Adjustments for switchmode script

--- static/scripts/switchmode.js
+++ static/scripts/switchmode.js
@@ -1,46 +1,46 @@
 (function () {
-  function initColorMode () {
-    function storageAvailable () {
-      var x = '__storage_test__';
-      try {
-        window.localStorage.setItem(x, x);
-        window.localStorage.removeItem(x);
-        return true;
-      }
-      catch (e) {
-        return e instanceof DOMException && (
-          e.code === 22 ||
-          e.code === 1014 ||
-          e.name === 'QuotaExceededError' ||
-          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-          (window.localStorage && window.localStorage.length !== 0);
-      }
+  function storageAvailable () {
+    var x = '__storage_test__';
+    try {
+      window.localStorage.setItem(x, x);
+      window.localStorage.removeItem(x);
+      return true;
     }
-    function getSwitchMode () {
-      if (storageAvailable() &&
-          window.localStorage.getItem('switchMode')) {
-        return window.localStorage.getItem('switchMode');
-      }
-      return 'auto';
+    catch (e) {
+      return e instanceof DOMException && (
+        e.code === 22 ||
+        e.code === 1014 ||
+        e.name === 'QuotaExceededError' ||
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+        (window.localStorage && window.localStorage.length !== 0);
     }
-    const determineColorMode = (switchMode) => {
-      switch (switchMode) {
-        case 'night':
+  }
+  function getSwitchMode () {
+    if (storageAvailable() &&
+      window.localStorage.getItem('switchMode')) {
+      return window.localStorage.getItem('switchMode');
+    }
+    return 'auto';
+  }
+  function determineColorMode (switchMode) {
+    switch (switchMode) {
+      case 'night':
+        return 'dark';
+      case 'auto':
+        if (window.matchMedia(
+          '(prefers-color-scheme: dark)').matches) {
           return 'dark';
-        case 'auto':
-          if (window.matchMedia(
-            '(prefers-color-scheme: dark)').matches) {
-            return 'dark';
-          }
-        default:
-          return 'light';
-      }
+        }
+      default:
+        return 'light';
     }
+  }
+  function initColorMode () {
     document.body.setAttribute('data-color-mode',
       determineColorMode(getSwitchMode()));
   }
   if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded',initColorMode, false);
+    document.addEventListener('DOMContentLoaded', initColorMode, false);
   } else {
     initColorMode();
   }
