26cc5d7479b1dd41e91e4de976dc4c37bab4c9f9
Edik <taikn@outlook.com>
2020-05-27 11:44:16 +0200

    Rename switchmode to colormode to avoid confusion

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const onRenderBody = ({ setHeadComponents, setBodyAttributes }) => {
   setHeadComponents([
     <script
-      key='/scripts/switchmode.js'
-      src='/scripts/switchmode.js'
+      key='/scripts/colormode.js'
+      src='/scripts/colormode.js'
     />
   ])
   setBodyAttributes({
