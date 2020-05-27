a0688a4766f8ab46617bdc7562ddcf6ae0d55d3f
Edik <taikn@outlook.com>
2020-05-27 20:52:08 +0200

    Add more content to the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,5 +18,389 @@ export default () => (
       <br />
       <SimpleLink to='/next-page/'>Next page</SimpleLink>
     </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
+    <PrimaryHeading>
+      Hello and Welcome!
+    </PrimaryHeading>
+    <BodyParagraph>
+      Here comes our long paragraph text. Fusce ultrices justo porttitor ipsum
+      pellentesque, a sodales augue eleifend. Sed non eros quam. Nulla lectus odio,
+      commodo in ultrices in, consectetur ut mauris. Nam mattis, augue sed fringilla
+      pellentesque, nibh velit vehicula lectus, sit amet vehicula mi massa a ante.
+      <br />
+      <br />
+      <SimpleLink to='/next-page/'>Next page</SimpleLink>
+    </BodyParagraph>
   </Layout>
 )
