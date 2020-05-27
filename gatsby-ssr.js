ecbdfa116097fee7a13de15ff1060d61e8c087fa
Edik <taikn@outlook.com>
2020-05-27 21:30:19 +0200

    Embed the colorMode function in body

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,52 @@
 import React from 'react'
 
-export const onRenderBody = ({ setHeadComponents, setBodyAttributes }) => {
-  setHeadComponents([
-    <script
-      key='/scripts/colormode.js'
-      src='/scripts/colormode.js'
-    />
-  ])
+const codeToRun = `(function () {
+  function storageAvailable () {
+    var x = '__storage_test__';
+    try {
+      window.localStorage.setItem(x, x);
+      window.localStorage.removeItem(x);
+      return true;
+    } catch (e) {
+      return e instanceof DOMException && (
+        e.code === 22 ||
+        e.code === 1014 ||
+        e.name === 'QuotaExceededError' ||
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+        (window.localStorage && window.localStorage.length !== 0);
+    }
+  }
+  function getSwitchMode () {
+    if (storageAvailable() &&
+      window.localStorage.getItem('switchMode')) {
+      return window.localStorage.getItem('switchMode');
+    }
+    return 'auto';
+  }
+  function determineColorMode (switchMode) {
+    switch (switchMode) {
+      case 'night':
+        return 'dark';
+      case 'auto':
+        if (window.matchMedia(
+          '(prefers-color-scheme: dark)').matches) {
+          return 'dark';
+        }
+      default:
+        return 'light';
+    }
+  }
+  function setInitColorMode () {
+    document.body.setAttribute('data-color-mode',
+      determineColorMode(getSwitchMode()));
+  }
+  setInitColorMode();
+})();`
+
+export const onRenderBody = ({ setPreBodyComponents, setBodyAttributes }) => {
+  setPreBodyComponents(
+    <script dangerouslySetInnerHTML={{ __html: codeToRun }} />
+  )
   setBodyAttributes({
     'data-loading': true
   })
